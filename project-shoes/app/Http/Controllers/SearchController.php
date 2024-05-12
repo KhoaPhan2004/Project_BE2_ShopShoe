f20f0f6cc4a00f7fdd0c0e3d8d909c1d64472db3
Haonguyen84 <haohoangnguyenbn@gmail.com>
2024-05-12 14:31:41 +0700

    timkiemt2dk

--- project-shoes/app/Http/Controllers/SearchController.php
+++ project-shoes/app/Http/Controllers/SearchController.php
@@ -12,9 +12,13 @@ class SearchController extends Controller
         $keyword = $request->input('keyword');
 
         // Tìm kiếm sản phẩm theo tên hoặc hãng
-        $products = Product::where('product_name', 'like', "%$keyword%")
-                            ->orWhere('price', 'like', "%$keyword%")
-                            ->get();
+        $products = Product::select('products.*', 'origins.origin_name AS origin')
+            ->join('origins', 'products.origin_id', '=', 'origins.id')
+            ->where(function ($query) use ($keyword) {
+                $query->where('products.product_name', 'like', "%$keyword%")
+                      ->orWhere('origins.origin_name', 'like', "%$keyword%");
+            })
+            ->get();
 
         // Trả về view hiển thị kết quả tìm kiếm
         return view('search_results', ['products' => $products]);
