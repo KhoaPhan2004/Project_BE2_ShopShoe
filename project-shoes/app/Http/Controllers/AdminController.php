ab737c39d80b4398463e6db9f1dc971778ed0cba
huynhvietcanh2207 <huynhvietcanh2004@gmail.com>
2024-05-13 22:36:56 +0700

    create login_admin

--- project-shoes/app/Http/Controllers/AdminController.php
+++ project-shoes/app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash; // Import Hash facade
 
 class AdminController extends Controller
@@ -12,5 +13,21 @@ class AdminController extends Controller
     {
         return view('admin.index');
     }
-    
+
+    public function login()
+    {
+        return view('admin.loginAdmin');
+    }
+    public function check_login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'role' => 1])) {
+            return redirect()->route('admin.index');
+        }
+        return redirect()->back()->with('err', 'Sai thông tin ');
+    }
+    public function singOut()
+    {
+       Auth::logout();
+       return redirect()->route('admin.login');
+    }
 }
